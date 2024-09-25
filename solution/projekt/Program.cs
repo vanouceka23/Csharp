8b08dfd6593a9255b5b21cc4adaf33758f1f9719
Antonín Vaňouček <vanouceka23@spstrutnov.cz>
2024-09-25 13:29:28 +0200

    kámen nůžky papír

--- solution/projekt/Program.cs
+++ solution/projekt/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string slovo;
+            int číslo;
+            Random random = new Random();
+            Console.Write("zadejte (kámen, nůžky, papír):");
+            slovo=Console.ReadLine();
+
+
+            if (slovo == "kámen"  || slovo =="nůžky" || slovo =="papír")
+            {
+                číslo = random.Next(1,4);
+                if (číslo == 1)
+                {
+                    Console.WriteLine("prohra");
+                }
+                else if (číslo == 2)
+                {
+                    Console.WriteLine("výhra");
+                }
+                else if (číslo == 3)
+                {
+                    Console.WriteLine("remíza");
+                }
+                else
+                {
+                    Console.WriteLine("něco se pokazilo");
+                }
+
+            }
+            else 
+            {
+                Console.WriteLine("špatně zadané");
+            }
+
+
         }
     }
 }
